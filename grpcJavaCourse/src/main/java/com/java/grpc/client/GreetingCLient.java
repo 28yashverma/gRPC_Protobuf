dc5108ea6203b054671de90e533be3a80fcf2a01
28yashverma <28.yashverma@gmail.com>
2020-05-27 16:29:45 +0530

    Streaming Server

--- grpcJavaCourse/src/main/java/com/java/grpc/client/GreetingCLient.java
+++ grpcJavaCourse/src/main/java/com/java/grpc/client/GreetingCLient.java
@@ -1,11 +1,8 @@
 package com.java.grpc.client;
 
 import com.proto.dummy.DummyServiceGrpc;
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
-import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.*;
 import com.proto.greet.GreetServiceGrpc.*;
-import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -24,17 +21,30 @@ public class GreetingCLient {
         //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
         GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
-        Greeting greeting = Greeting.newBuilder()
-                .setFirstName("Yeshendra")
-                .setLastName("Verma")
+
+        // Unary call
+//        Greeting greeting = Greeting.newBuilder()
+//                .setFirstName("Yeshendra")
+//                .setLastName("Verma")
+//                .build();
+//
+//        GreetRequest request = GreetRequest.newBuilder()
+//                .setGreeting(greeting)
+//                .build();
+//        GreetResponse response = greetClient.greet(request);
+//        System.out.println(response.getResult());
+
+        // Server streaming call
+        // we prepare the request here
+        GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest
+                .newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("Yeshendra"))
                 .build();
 
-        GreetRequest request = GreetRequest.newBuilder()
-                .setGreeting(greeting)
-                .build();
-        GreetResponse response = greetClient.greet(request);
-        System.out.println(response.getResult());
-        //do something
+        //we stream the result
+        greetClient.greetManyTimes(greetManyTimesRequest).forEachRemaining(response -> {
+            System.out.println(response.getResult());
+        });
 
         System.out.println("Shutting down channel");
         channel.shutdown();
